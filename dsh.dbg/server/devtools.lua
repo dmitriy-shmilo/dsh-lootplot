596f1891c1453c8f7d80459a5b4d0c25d0ab6198
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2025-12-05 19:40:22 +0200

    DBG - Initial commit.

---
+++ dsh.dbg/server/devtools.lua
@@ -0,0 +1,113 @@
+local SPIRAL_STEPS = 45
+
+-- taken from lootplot.singleplayer/shared/devtools.lua
+local function getPPos(clientId)
+    local ctx = assert(lp.singleplayer.getRun())
+    local plot = ctx:getPlot()
+
+    local player = control.getControlledEntities(clientId)[1]
+    return plot:getClosestPPos(player.x, player.y)
+end
+
+
+-- spawn an item at the center of the screen or a nearby suitable slot
+-- TODO: implement an entity name lookup
+chat.handleCommand("si", {
+    adminLevel = 120,
+    arguments = {
+        {name = "entityType", type = "string"},
+    },
+    handler = function(clientId, etype)
+        if not server then
+            return
+        end
+
+        local ent = server.entities[etype]
+        if (not ent) then
+            chat.privateMessage(clientId, tostring(etype) .. " entity type doesn't exist.")
+            return
+        end
+
+        if (not lp.isItemEntity(ent)) then
+            chat.privateMessage(clientId, tostring(etype) .. " is not an item entity type.")
+            return
+        end
+
+        -- adapted from https://jonseymour.medium.com/investigating-the-properties-of-a-square-spiral-6aa635a4d803
+        local cursor = getPPos(clientId)
+        local motions = {
+            cursor.right,
+            cursor.up,
+            cursor.left,
+            cursor.down
+        }
+        local motionIdx = 0
+        local stepCount = SPIRAL_STEPS
+        local extent = 0
+
+        while stepCount > 0 do
+            if motionIdx == 0 or motionIdx == 2 then
+                extent = extent + 1
+            end
+            local leg = math.min(extent, stepCount)
+            stepCount = stepCount - leg
+            while leg > 0 do 
+                cursor = motions[motionIdx + 1](cursor)
+                leg = leg - 1
+
+                local slotEnt = lp.posToSlot(cursor)
+
+                if slotEnt or ent.canItemFloat then
+                    if lp.forceSpawnItem(cursor, ent, lp.singleplayer.PLAYER_TEAM) then
+                        chat.privateMessage(clientId, "Spawned item at " .. tostring(cursor))
+                        return
+                    end
+                end
+            end
+            motionIdx = (motionIdx + 1) % #motions
+        end
+
+        chat.privateMessage(clientId, "Can not spawn " .. etype .. ": can't find a suitable nearby slot.")
+    end
+})
+
+-- spawn a slot at the center of the screen, replacing any slot that was there previously
+-- accepts, optionally, a slot type name (string, defaults to "slot") and size (number, defaults to 0)
+chat.handleCommand("ss", {
+    adminLevel = 120,
+    arguments = {},
+    handler = function(clientId, arg1, arg2)
+        local slotType = "slot"
+        local size = 0
+        if arg1 then
+            if arg2 then
+                slotType = arg1
+                size = tonumber(arg2) or 0
+            elseif tonumber(arg1) then
+                size = tonumber(arg1) or 0
+            else
+                slotType = arg1
+            end
+        end
+
+        local ctor = server.entities[slotType]
+        if (not ctor) then
+            chat.privateMessage(clientId, "Can not spawn " .. slotType .. ": no such slot type.")
+            return
+        end
+
+        if (not lp.isSlotEntity(ctor)) then
+            chat.privateMessage(clientId, "Can not spawn " .. slotType .. ": not a slot type.")
+        end
+
+        local ppos = getPPos(clientId)
+        ppos:move(-size, -size)
+
+        for x = -size, size do
+            for y = -size, size do
+                local cursor = ppos:move(x, y)
+                lp.forceSpawnSlot(cursor, ctor, lp.singleplayer.PLAYER_TEAM)
+            end
+        end
+    end
+})
